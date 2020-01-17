a360d11b52eedb1671d8b415e5f75111ccbcd803
David França <davidemanuel.cf@gmail.com>
2020-01-17 16:48:19 -0300

    Aplicativo React Native

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,176 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
+import MapView, { Marker, Callout } from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import api from "../services/api";
+
+function Main({ navigation }) {
+  const [devs, setDevs] = useState([]);
+  const [currentRegion, setCurrentRegion] = useState(null);
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.03
+        });
+      }
+    }
+    loadInitialPosition();
+  }, []);
+
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get("/search", {
+      params: {
+        latitude,
+        longitude,
+        techs: "Docker"
+      }
+    });
+
+    setDevs(response.data);
+  }
+
+  function handleRegionChanges(region) {
+    console.log(region);
+    setCurrentRegion(region);
+  }
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <>
+      <MapView
+        onRegionChangeComplete={handleRegionChanges}
+        initialRegion={currentRegion}
+        style={styles.map}
+      >
+        <Marker coordinate={{ latitude: -3.8580372, longitude: -38.4955035 }}>
+          <Image
+            style={styles.avatar}
+            source={{
+              uri: "https://avatars0.githubusercontent.com/u/30361358?s=460&v=4"
+            }}
+          />
+          <Callout
+            onPress={() => {
+              navigation.navigate("Profile", {
+                github_username: "francinha02"
+              });
+            }}
+          >
+            <View style={styles.callout}>
+              <Text style={styles.devName}>David França</Text>
+              <Text style={styles.devBio}>
+                Entusiasta e amante da tecnologia. Analista de Sistemas e
+                Programador Freelancer.
+              </Text>
+              <Text style={styles.devTechs}>NodeJS, Ionic, JavaScript</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por techs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
+          <MaterialIcons name="my-location" syze={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: "#FFF"
+  },
+  callout: {
+    width: 260
+  },
+  devName: {
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  devBio: {
+    color: "#666",
+    marginTop: 5
+  },
+  devTechs: {
+    marginTop: 5
+  },
+  searchForm: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: "row"
+  },
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#FFF",
+    color: "#333",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  },
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#8E4DFF",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 15
+  }
+});
+
+export default Main;
